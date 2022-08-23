32be583f66d1c935761254916c339d287ac49b11
서유진 <youjin030700@gmail.com>
2022-08-24 01:26:28 +0900

    feat: 채용공고 상세보기 API
    (fn)addAnotherRecruit : 선택된 채용공고 회사의 타 채용공고를 조회하여 another_recruit항목에 배열로 추가
    
    req.params로 받아온 채용공고 정보의 상세 내용과, 해당 채용공고를 올린 회사의 타 채용공고 내용을 응답으로 받아온다.

--- routes/apply.js
+++ routes/apply.js
@@ -23,6 +23,24 @@ const addCompanyInfoAll = async (array) => {
     return array;
 }
 
+const addAnotherRecruit = async (array) => {
+    let allRecruitId = await Recruit.findAll({attributes: ['recruit_id']},{
+        where: {
+            company_id : array.dataValues.company_id
+            }
+        });
+    console.log("allRecruitId",allRecruitId);
+
+    let ids = []
+    for (const item of allRecruitId) {   
+        ids.push(item.dataValues.recruit_id)    
+    }
+
+    array.dataValues['another_recruit'] = ids;
+    
+    return array;
+}
+
 
 router.get('/all',async (req,res,next)=>{
     // 모든 채용공고 조회
@@ -33,6 +51,33 @@ router.get('/all',async (req,res,next)=>{
     allRecruit = await addCompanyInfoAll(allRecruit)
     console.log(allRecruit)
     res.status(200).json({allRecruit});
+});
+
+
+
+router.get('/:id/detail',async (req,res,next)=>{
+    // 채용공고 상세보기
+    
+    let theRecruit = await Recruit.findOne(
+        { where: {
+            recruit_id : req.params.id
+            }
+        })
+    .catch((err) => {
+        return next(err);
+    });
+    if(!theRecruit){
+        return res.status(500).json({message:"채용공고 정보를 확인해주세요"});
+    }
+
+    // 회사정보추가 
+    theRecruit = await addCompanyInfo(theRecruit);
+
+     // 회사의 다른 채용공고 추가
+    theRecruit = await addAnotherRecruit(theRecruit);
+
+    res.status(200).json({theRecruit});
+
 });
 
 

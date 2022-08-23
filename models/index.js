f9e0d2649ee618488eae903928e0249cd06043b2
서유진 <youjin030700@gmail.com>
2022-08-24 01:07:56 +0900

    feat: 모델 관계 설정
    모델간의 관계를 설정한다.
    Recruit : User ==> 1:n
     하나의 채용공고에는 다양한 사용자가 지원할 수 있다.
    Company : Recruit ==> 1:n
     하나의 회사는 여러개의 채용공고를 등록할 수 있다.

--- models/index.js
+++ models/index.js
@@ -8,6 +8,17 @@ const Company = require('./company.model')(sequelize);
 const Recruit = require('./recruit.model')(sequelize);
 
 
+// Recruit : User ==> 1:n 
+Recruit.hasMany(User, {foreignKey: 'recruit_id'});
+User.belongsTo(Recruit,{foreignKey: 'recruit_id'});
+
+
+// Company : Recruit ==> 1:n 
+Company.hasMany(Recruit, {foreignKey: 'company_id'});
+Recruit.belongsTo(Company,{foreignKey: 'company_id'});
+
+
+
 const db = {};
 db.sequelize = sequelize;
 

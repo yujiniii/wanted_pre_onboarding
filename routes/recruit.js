4b8ee84156654a4ea150d6045e4d53b72e1096f1
서유진 <youjin030700@gmail.com>
2022-08-24 01:16:39 +0900

    feat: 채용공고 수정
    company_id를 제외한 다른 값을 req.body로 받아와 수정한다.
    수정 후 수정된 정보를 조회하여 응답으로 날려준다.

--- routes/recruit.js
+++ routes/recruit.js
@@ -17,4 +17,26 @@ router.post('/new', async (req,res,next)=>{
 });
 
 
+router.post('/modify',async (req,res,next)=>{
+    // 채용공고 수정
+    await Recruit.update({
+        position:req.body.position,
+        reward: req.body.reward,
+        skill:req.body.skill,
+        content:req.body.content,
+    },{
+        where:{
+            recruit_id:req.body.recruit_id
+        }
+    });
+    let change = await Recruit.findOne({
+        where: {
+            recruit_id:req.body.recruit_id
+        }
+    });
+    res.status(200).json({change});
+    console.log('채용공고 수정');
+});
+
+
 module.exports = router;
